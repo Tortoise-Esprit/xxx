6624c588533f5ff9120a0feacbfb9232a93348be
Esprit <jquma@me.com>
2026-07-13 01:09:09 +0900

    Register gallery batch DSC_0813 to DSC_0835

--- gallery-index.ts
+++ gallery-index.ts
@@ -8,6 +8,7 @@ import { galleryBatch0710To0738 } from "./gallery-batches/gallery_0710_0738";
 import { galleryBatch0739To0758 } from "./gallery-batches/gallery_0739_0758";
 import { galleryBatch0759To0781 } from "./gallery-batches/gallery_0759_0781";
 import { galleryBatch0782To0812 } from "./gallery-batches/gallery_0782_0812";
+import { galleryBatch0813To0835 } from "./gallery-batches/gallery_0813_0835";
 
 export const allGallery = [
   ...galleryBatch0275To0424,
@@ -20,4 +21,5 @@ export const allGallery = [
   ...galleryBatch0739To0758,
   ...galleryBatch0759To0781,
   ...galleryBatch0782To0812,
+  ...galleryBatch0813To0835,
 ];
