e408b8063704ccff59184f0b16bf3a63943ea0e7
Esprit <jquma@me.com>
2026-07-12 17:49:52 +0900

    Add combined gallery index

---
+++ gallery-index.ts
@@ -0,0 +1,7 @@
+import { gallery } from "./gallery";
+import { galleryBatch0427To0561 } from "./gallery-batches/gallery_0427_0561";
+
+export const allGallery = [
+  ...gallery,
+  ...galleryBatch0427To0561,
+];
