87396d45b16f7897d4d9a2b2821371ab73679958
Esprit <jquma@me.com>
2026-07-12 22:05:42 +0900

    Include gallery batches through DSC_0738

--- gallery-index.ts
+++ gallery-index.ts
@@ -3,6 +3,8 @@ import { galleryBatch0427To0561 } from "./gallery-batches/gallery_0427_0561";
 import { galleryBatch0571To0632 } from "./gallery-batches/gallery_0571_0632";
 import { galleryBatch0633To0661 } from "./gallery-batches/gallery_0633_0661";
 import { galleryBatch0662To0684 } from "./gallery-batches/gallery_0662_0684";
+import { galleryBatch0685To0709 } from "./gallery-batches/gallery_0685_0709";
+import { galleryBatch0710To0738 } from "./gallery-batches/gallery_0710_0738";
 
 export const allGallery = [
   ...galleryBatch0275To0424,
@@ -10,4 +12,6 @@ export const allGallery = [
   ...galleryBatch0571To0632,
   ...galleryBatch0633To0661,
   ...galleryBatch0662To0684,
+  ...galleryBatch0685To0709,
+  ...galleryBatch0710To0738,
 ];
