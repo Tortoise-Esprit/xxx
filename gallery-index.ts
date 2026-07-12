99c9b26e45c197161441d5caf7727d16ce062ba6
Esprit <jquma@me.com>
2026-07-12 21:03:47 +0900

    Include gallery batch DSC_0662 to DSC_0684

--- gallery-index.ts
+++ gallery-index.ts
@@ -2,10 +2,12 @@ import { gallery as galleryBatch0275To0424 } from "./gallery-batches/gallery_027
 import { galleryBatch0427To0561 } from "./gallery-batches/gallery_0427_0561";
 import { galleryBatch0571To0632 } from "./gallery-batches/gallery_0571_0632";
 import { galleryBatch0633To0661 } from "./gallery-batches/gallery_0633_0661";
+import { galleryBatch0662To0684 } from "./gallery-batches/gallery_0662_0684";
 
 export const allGallery = [
   ...galleryBatch0275To0424,
   ...galleryBatch0427To0561,
   ...galleryBatch0571To0632,
   ...galleryBatch0633To0661,
+  ...galleryBatch0662To0684,
 ];
